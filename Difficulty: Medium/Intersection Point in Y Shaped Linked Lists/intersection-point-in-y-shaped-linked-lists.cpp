4900be533c22871768a6a279e2ecb1e13e12bd8c
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2025-09-14 21:36:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.cpp
@@ -0,0 +1,36 @@
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution {
+  public:
+    Node* intersectPoint(Node* head1, Node* head2) {
+        // Your Code Here
+        unordered_map<Node *,bool>mpp;
+    
+    Node *temp = head1;
+    
+    while(temp != NULL)
+    {
+        mpp[temp] = true;
+        temp = temp->next;
+    }
+    
+    temp = head2;
+    
+    while(temp != NULL){
+       if(mpp[temp] == true){
+           return temp;
+       }
+       temp = temp->next;
+    }
+    
+    return NULL;
+    }
+};
