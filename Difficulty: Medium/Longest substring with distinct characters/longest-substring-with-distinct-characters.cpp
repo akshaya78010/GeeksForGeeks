c600f81c0239bccba5bfa91f590afe7ff1cb2614
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2025-10-15 23:49:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest substring with distinct characters/longest-substring-with-distinct-characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int longestUniqueSubstr(string &s) {
+        // code here
+         map<char,int>mpp;
+        
+        int i = 0;
+        int j = 0;
+        int n = s.size();
+        int longest = 0;
+       while(j < n)
+       {mpp[s[j]]+=1;
+        //   longest = max(longest,(j-i+1));
+           
+               while(mpp[s[j]] > 1)
+               {
+                   mpp[s[i]]-=1;
+                   i+=1;
+               }
+           
+           
+            longest = max(longest,(j-i+1));
+           j+=1;
+           
+       }
+       
+       return longest;
+    }
+};
