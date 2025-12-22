da2ec89cd96f3c4a2828cfc876363baf1930f88c
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2025-12-22 13:35:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -3,28 +3,24 @@ class Solution {
   public:
     int rowWithMax1s(vector<vector<int>> &arr) {
         // code here
-        
-        int n = arr.size();
+        int maxi = 0;
+        int max_ind = -1;
         int county = 0;
-        int maxi = INT_MIN;
-        int ind = -1;
-        for(int i =0; i < arr.size() ; i++)
-        {
+        
+        for(int i =0; i < arr.size() ; i++){
             county = 0;
-            for(int j =0; j < arr[i].size() ; j++)
-            {
-                if(arr[i][j] == 1)
-                {
-                    county += 1;
-                }
+            for(int j =0; j < arr[i].size() ; j++){
+             if(arr[i][j] == 1){
+                 county += 1;
+             }   
             }
-           if(county > maxi)
-           {
-               maxi = county;
-               ind = i;
-           }
+            
+            if(county > maxi){
+                maxi = county;
+                max_ind = i;
+            }
         }
         
-        return ind;
+       return max_ind; 
     }
 };
